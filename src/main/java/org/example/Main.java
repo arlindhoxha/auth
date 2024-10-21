7dd9f002ebc7dd0a6d0a58c8a79af68c29baf1ee
arlindhoxha <arlindhoxha007@gmail.com>
2024-10-21 14:16:57 -0400

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/**
+ * Created by Arlind Hoxha on 10/20/2024.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
